46c938fa97587b4e5c33ffbf05dca3a5b390897f
JuanAlv526 <114698208+JuanAlv526@users.noreply.github.com>
2024-02-28 19:18:11 -0500

    warnings

--- src/worms_mech/worms_mech/gait_parser_actions.py
+++ src/worms_mech/worms_mech/gait_parser_actions.py
@@ -18,19 +18,25 @@ class JointCommandPublisher(Node):
 
         # Define waypoints (motor commands as positions here)
         # TODO: NEGATE EVERYTHING BC IT IS NOW RUNNING ON FR
+
+        #ZERO INITIAL POSITION ALWAYS!!!!!
         self.br_step_waypoints = [
+            #ZERO INITIAL POSITION ALWAYS!!!!!
+            [0, 0, 0],
             [0, 145, 175], 
             [30, 145, 175], #Dead Position to Prob Position
             [30, 135, 175]
               
         ]
         self.br_prone_waypoints = [
+            #ZERO INITIAL POSITION ALWAYS!!!!!
             [0, 0, 0],
             [0, -120, 175]
             
         ]
 
         self.br_lift_waypoints = [
+            #ZERO INITIAL POSITION ALWAYS!!!!!
             [0, 0, 0],
             [30, -45, 35]
             
