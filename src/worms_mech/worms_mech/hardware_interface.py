8cb0b50843daf4966f49a8a1d099cc0098ebb206
Jacob Rodriguez <jacobrod@mit.edu>
2024-03-04 15:57:55 -0500

    STUPID 3

--- src/worms_mech/worms_mech/hardware_interface.py
+++ src/worms_mech/worms_mech/hardware_interface.py
@@ -164,7 +164,7 @@ class MotorControllerNode(Node):
                     self.motor_reported_pos3, self.vel3, self.curr3 = motor_controller.send_deg_command(logic_pose_command * self.motor3_direction, vel_command, Kp, Kd, K_ff)
                     self.logical_pos3 = self.motor_reported_pos3 * self.motor3_direction
 
-                    print("Motor 1 Commanded To: " + str(logic_pose_command))
+                    print("Motor 3 Commanded To: " + str(logic_pose_command))
 
                     if(self.logical_pos3 == None):
                         self.worm_heartbeat.data = "Disabled"
