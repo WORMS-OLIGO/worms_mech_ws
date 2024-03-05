dcef3454c3ba00b909ea11be0eac304a8745cb21
Jacob Rodriguez <jacobrod@mit.edu>
2024-03-05 15:50:21 -0500

    Added slower timer

--- src/worms_mech/worms_mech/gait_manager.py
+++ src/worms_mech/worms_mech/gait_manager.py
@@ -168,7 +168,7 @@ class JointCommandPublisher(Node):
         self.position_index = 0
         self.current_position = [0, 0, 0]
 
-        self.timer = self.create_timer(0.1, self.timer_callback)
+        self.timer = self.create_timer(0.2, self.timer_callback)
 
     def joint_state_callback(self, msg):
         
