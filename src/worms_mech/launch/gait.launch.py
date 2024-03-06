21b9e915927ece9c91c567a2b51589ee5d471588
JuanAlv526 <114698208+JuanAlv526@users.noreply.github.com>
2024-03-05 22:27:22 -0500

    change launch

--- src/worms_mech/launch/gait.launch.py
+++ src/worms_mech/launch/gait.launch.py
@@ -5,20 +5,21 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='worms_mech',
-            executable='hardware_interface',
-            namespace="",
-            name='hardware_interface',
-            # Launch the node with root access (GPIO) in a shell
-            prefix=["sudo -E env \"PYTHONPATH=$PYTHONPATH\" \"LD_LIBRARY_PATH=$LD_LIBRARY_PATH\" \"PATH=$PATH\" \"USER=$USER\"  bash -c "],
-            shell=True,
-        ),
-        Node(
-            package='worms_mech',
             executable='gait_manager',
             namespace="",
             name='gait_manager',
             # Launch the node with root access (GPIO) in a shell
             prefix=["sudo -E env \"PYTHONPATH=$PYTHONPATH\" \"LD_LIBRARY_PATH=$LD_LIBRARY_PATH\" \"PATH=$PATH\" \"USER=$USER\"  bash -c "],
             shell=True,
+        ),
+        Node(
+            package='worms_mech',
+            executable='hardware_interface',
+            namespace="",
+            name='hardware_interface',
+            # Launch the node with root access (GPIO) in a shell
+            prefix=["sudo -E env \"PYTHONPATH=$PYTHONPATH\" \"LD_LIBRARY_PATH=$LD_LIBRARY_PATH\" \"PATH=$PATH\" \"USER=$USER\"  bash -c "],
+            shell=True,
         )
+        
     ])
