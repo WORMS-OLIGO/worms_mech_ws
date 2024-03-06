62c13d02c43e7fe0a3dd02c58e68eefd898eaadc
JuanAlv526 <114698208+JuanAlv526@users.noreply.github.com>
2024-03-05 21:55:47 -0500

    made gait launch file

---
+++ src/worms_mech/launch/gait.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='worms_mech',
+            executable='hardware_interface',
+            namespace="",
+            name='hardware_interface',
+            # Launch the node with root access (GPIO) in a shell
+            prefix=["sudo -E env \"PYTHONPATH=$PYTHONPATH\" \"LD_LIBRARY_PATH=$LD_LIBRARY_PATH\" \"PATH=$PATH\" \"USER=$USER\"  bash -c "],
+            shell=True,
+        ),
+        Node(
+            package='worms_mech',
+            executable='gait_manager',
+            namespace="",
+            name='gait_manager',
+            # Launch the node with root access (GPIO) in a shell
+            prefix=["sudo -E env \"PYTHONPATH=$PYTHONPATH\" \"LD_LIBRARY_PATH=$LD_LIBRARY_PATH\" \"PATH=$PATH\" \"USER=$USER\"  bash -c "],
+            shell=True,
+        )
+    ])
