aea7abfa4c84d6c65c6e62f89dbe59ab5a560325
Jacob Rodriguez <jacobrod@mit.edu>
2024-03-13 19:22:00 -0400

    Changed poses

--- bootup.sh
+++ bootup.sh
@@ -3,5 +3,7 @@ sudo ip link set up can0
 source /opt/ros/humble/setup.bash
 cd ~/worms_mech_ws/
 source install/setup.bash
+ros2 daemon stop 
+ros2 daemon start
 ros2 launch worms_mech camera.launch.py
 
